c240992ce241bddfdc8457e1b590849ffdab3df2
Archana Dash <iarchanadash@gmail.com>
2022-02-13 02:24:28 +0530

    Added source code

--- fairweather/weatherapp/views.py
+++ fairweather/weatherapp/views.py
@@ -1,27 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
 import urllib.request
 import json
+from django.shortcuts import render
+
 
 def index(request):
+
     if request.method == 'POST':
         city = request.POST['city']
-        source = urllib.request.urlopen('api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=fbafe219cc1f16e3915ae457b6594866').read()
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=fbafe219cc1f16e3915ae457b6594866').read()
         list_of_data = json.loads(source)
+
         data = {
-            "country_code" : str(list_of_data(['sys']['country'])),
-            "coordinate" : str(list_of_data(['coord']['lon'])) + ','+ str(list_of_data(['coord']['lat'])),
-            "temperature" : str(list_of_data(['main']['temp'])) + ' ℃',
-            "pressure" : str(list_of_data(['main']['pressure']))
-            "humidity" : str(list_of_data(['main']['humidity'])),
-            "main" :str(list_of_data['weather'][0]['main']),
-            "wind speed" : str(list_of_data(['wind']['speed'])),
-            "description" : str(list_of_data(['weather'][0]['description'])),
-            "icon" : list_of_data(['weather'][0]['icon']),
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            "windspeed": str(list_of_data['wind']['speed']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
         }
         print(data)
-        else:
-            data={}
-        return render(request,'index.html',data)
-        
+    else:
+        data = {}
+
+    return render(request, "weatherapp/index.html", data)
