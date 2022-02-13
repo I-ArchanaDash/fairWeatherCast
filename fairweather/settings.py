8a109d08120fb5339a1c0d21f51fc18ffe6f0ce2
Archana Dash <iarchanadash@gmail.com>
2022-02-13 16:20:50 +0530

    source code updation

--- fairweather/settings.py
+++ fairweather/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR= os.path.join(BASE_DIR, 'static')
@@ -126,7 +126,11 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = (
     STATIC_DIR,
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
